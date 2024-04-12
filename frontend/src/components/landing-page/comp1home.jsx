f91e4f0053ba7593a63591d89906274d7fef2d9a
Radhika0024 <radhikasharma03022002@gmail.com>
2024-04-12 21:51:39 +0530

    home page comp 1 & 2 and related css and photos addded

---
+++ frontend/src/components/landing-page/comp1home.jsx
@@ -0,0 +1,18 @@
+import '../../assets/css/comp1home.css'
+const Comp1home =()=>{
+    return(<>
+         <div className="homebg">
+           <div className="home-bgTxt">Welcome to Farmer Solution</div>
+           <div className="home-bgTxt1" >Innovating Facilities for</div>
+           <div className="home-bgTxt1 ">Tommorow's Farms</div>
+          
+           <div className="homebtn"><button type="button" class="btn btn-success">Discover More</button>
+           </div>
+        </div>
+
+        
+        </>
+    )
+}
+
+export default Comp1home;
