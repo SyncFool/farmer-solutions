97a90f72fb3c3b0704e10990ac1349b0d2ee2fbe
Radhika0024 <radhikasharma03022002@gmail.com>
2024-04-13 23:14:34 +0530

    comp 2 and comp 3 of home page related css , images

--- frontend/src/components/landing-page/comp2home.jsx
+++ frontend/src/components/landing-page/comp2home.jsx
@@ -1,5 +1,13 @@
 import "../../assets/css/comp2home.css"
 import { FcCloseUpMode } from "react-icons/fc";
+import { FaNutritionix } from "react-icons/fa6";
+import { GiFarmer } from "react-icons/gi";
+import { GiWheat } from "react-icons/gi";
+import { FaPeopleGroup } from "react-icons/fa6";
+
+
+
+
 
  const Comp2home=()=>{
     return (
@@ -10,36 +18,41 @@ import { FcCloseUpMode } from "react-icons/fc";
             <div className="sec2-right1"><FcCloseUpMode /> About Farmer</div>
             <div className="sec2-right2">Farmer & Agriculture</div>
             <div className="sec2-right-content"> Empowering farmers with streamlined tools, collaborative resources, and essential facilities for collective growth in agriculture. Revolutionizing farming with efficiency and empowerment, our platform fosters innovation and connectivity within the agricultural community.</div>
+            <div className="sec2-right4-main">
             <div className="sec2-right4">
-                <div className="sec2-right4-icon">icon</div>
+                <div className="sec2-right4-icon"><FaNutritionix />
+</div>
                 <div className="sec2-right4-content">
                     <div className="sec2-right4-heading">Crop Nutrition & fertilizer</div>
-                    <div className="sec2-right-content">platform offers resources and guidance to help farmers optimize crop nutrition</div>
+                    <div className="sec2-right-content">Platform offers resources and guidance to help farmers optimize crop nutrition , provide seeds and fertilizer also.</div>
                 </div>
             </div>
             <div className="sec2-right4">
-                <div className="sec2-right4-icon">icon</div>
+                <div className="sec2-right4-icon"><GiFarmer />
+</div>
                 <div className="sec2-right4-content">
                     <div className="sec2-right4-heading">Farm & Tool</div>
-                    <div className="sec2-right-content">Simplifying daily tasks for farmers by providing them with intuitive tools tailored to their needs.</div>
+                    <div className="sec2-right-content">Farmers to  manage their operation & Simplifies daily tasks for farmers by providing them  tools needed for farming.</div>
                 </div>
             </div>
             <div className="sec2-right4">
-                <div className="sec2-right4-icon">icon</div>
+                <div className="sec2-right4-icon"><GiWheat />
+</div>
                 <div className="sec2-right4-content">
                     <div className="sec2-right4-heading">Agriculture & Food</div>
-                    <div className="sec2-right-content"> we empower farmers by facilitating connections and providing access to a wider audience.</div>
+                    <div className="sec2-right-content">Empower  farmers to expand their marketing by facilitating connections and providing access to a wider audience.</div>
                 </div>
             </div>
             <div className="sec2-right4">
-                <div className="sec2-right4-icon">icon</div>
+                <div className="sec2-right4-icon"><FaPeopleGroup/></div>
                 <div className="sec2-right4-content">
                     <div className="sec2-right4-heading">Crops & Labour</div>
-                    <div className="sec2-right-content">simplifies the process of hiring workers, connecting farmers with skilled labor to support various farming tasks.</div>
+                    <div className="sec2-right-content">Simplifies the process of hiring workers and connecting farmers with skilled labor to support various farming tasks.</div>
                 </div>
             </div>
                
 
+            </div>
             </div>
             <div className="sec2-left">
                 <div className="sec2-left-base" ><img src="/assets/images/comp-3-img.jpg"/>
--- frontend/src/components/pages/Home.jsx
+++ frontend/src/components/pages/Home.jsx
@@ -3,6 +3,9 @@ import Navbar from "../Navbar.jsx";
 import Comp1 from "../landing-page/comp1.jsx";
 import Comp1home from "../landing-page/comp1home.jsx";
 import Comp2home from "../landing-page/comp2home.jsx";
+import Comp3home from "../landing-page/comp3home.jsx";
+import Comp4home from "../landing-page/comp4home.jsx";
+
 import Footer from "../Footer.jsx";
 
 // import "../../assets/css/Home.css";
@@ -16,6 +19,8 @@ const Home = () => {
             <Navbar />
             <Comp1home/>
             <Comp2home/>
+            <Comp3home/>
+            <Comp4home/>
             {/* <Comp1 /> */}
             <Footer />
         </>
