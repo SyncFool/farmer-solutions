f42c25ee2f78027344ff1a7017ea55ff74bd4b5e
Kamya <153488947+Kamya1234@users.noreply.github.com>
2024-04-14 16:53:15 +0530

    About and Project page and its css defined

---
+++ frontend/src/components/pages/About.jsx
@@ -0,0 +1,22 @@
+import Header from "../Header.jsx"
+import Footer from "../Footer.jsx"
+import Navbar from "../Navbar.jsx"
+import About1 from "../about/About-C1.jsx"
+// import About2 from "../about/About-C2.jsx"
+import About3 from "../about/About-C3.jsx"
+import Comp2home from "../landing-page/comp2home.jsx";
+
+export default function AboutUs(){
+    return(
+        <>
+        <Header/>
+        <Navbar/>
+        <About1/>
+        <Comp2home/>
+        <About3/>
+
+        <Footer/>
+        </> 
+    )
+}
+
---
+++ frontend/src/components/pages/Projects.jsx
@@ -0,0 +1,19 @@
+import Header from "../Header.jsx"
+import Footer from "../Footer.jsx"
+import Navbar from "../Navbar.jsx"
+import  Project1  from "../Project/Project.jsx"
+
+
+export default function Projects(){
+    return(
+        <>
+        <Header/>
+        <Navbar/>
+        <Project1/>
+        
+
+        <Footer/>
+        </> 
+    )
+}
+
