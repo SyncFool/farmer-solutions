27b6afc10e42525ba9bf501ad7354aab94b3b05b
Kamya <153488947+Kamya1234@users.noreply.github.com>
2024-04-15 10:09:52 +0530

    Aboutc4, AboutC5 and AboutC6 and its css defined

---
+++ frontend/src/components/about/About-C5.jsx
@@ -0,0 +1,22 @@
+import "../../assets/css/About-C5.css";
+
+const About5=()=>{
+    return (
+
+        
+
+        <div className="Im">
+        <div className="Tx">
+      
+           
+            <div>Empowering farmers is not just about providing solutions; it's about fostering resilience, sustainability, and prosperity from the ground up</div>
+        </div>
+
+     
+       </div>
+
+
+    )
+}
+
+export default About5;
--- frontend/src/components/pages/About.jsx
+++ frontend/src/components/pages/About.jsx
@@ -4,6 +4,9 @@ import Navbar from "../Navbar.jsx"
 import About1 from "../about/About-C1.jsx"
 // import About2 from "../about/About-C2.jsx"
 import About3 from "../about/About-C3.jsx"
+import About5 from "../about/About-C5.jsx"
+import About4 from "../about/About-C4.jsx"
+import About6 from "../about/About-C6.jsx"
 import Comp2home from "../landing-page/comp2home.jsx";
 
 export default function AboutUs(){
@@ -13,7 +16,11 @@ export default function AboutUs(){
         <Navbar/>
         <About1/>
         <Comp2home/>
+        <About5/>
+        <About4/>
+        <About6/>
         <About3/>
+        
 
         <Footer/>
         </> 
